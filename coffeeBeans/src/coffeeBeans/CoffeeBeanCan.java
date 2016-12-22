ea6d59b6cd954b3e0375f094338459258f69bb32
marinaesca <marinaescalante@utexas.edu>
2016-12-22 02:48:34 -0600

    almost complete, still need to fix errors, no graphics yet

--- coffeeBeans/src/coffeeBeans/CoffeeBeanCan.java
+++ coffeeBeans/src/coffeeBeans/CoffeeBeanCan.java
@@ -1,4 +1,7 @@
 // marinaescalante-unfinished
+
+// go back and add in enums for black and white beans?
+
 package coffeeBeans;
 
 import java.util.Random;
@@ -7,7 +10,7 @@ public class CoffeeBeanCan
 {
 	private int black;
 	private int white;
-	private int beans;
+	private char[] beans;
 
 	// empty constructor creates a rando can
 	// size is 10-100
@@ -20,9 +23,9 @@ public class CoffeeBeanCan
 		Random rand2 = new Random();
 		int n = rand2.nextInt(size);
 
-		this.white = n;
-		this.black = size - n;
-		this.beans = size;
+		this.black = n;
+		this.white = size - n;
+		this.beans = setBeans();
 
 	}
 
@@ -34,34 +37,145 @@ public class CoffeeBeanCan
 		Random rand = new Random();
 		int n = rand.nextInt(size);
 
-		this.white = n;
-		this.black = size - n;
-		this.beans = size;
+		this.black = n;
+		this.white = size - n;
+		this.beans = setBeans();
 	}
 
 	// empty constructor creates a rando can
 	// size is white+black
 	// colors are parameters
-	public CoffeeBeanCan(int white, int black)
+	public CoffeeBeanCan(int black, int white)
 	{
-		this.white = white;
-		this.black = black;
-		this.beans = white + black;
+		this.black = white;
+		this.white = black;
+		this.beans = setBeans();
 	}
 
-	public int getWhite()
+	public int getWhite() // this will be for drawing/ display purposes
 	{
 		return this.white;
 	}
 
-	public int getBlack()
+	public int getBlack() // this will be for drawing/ display purposes
 	{
 		return this.black;
 	}
 
-	public int getBeans()
+	public boolean oneLeft()
+	{
+		return (getBlack() + getWhite()) == 1;
+	}
+
+	public String getLastBean()
+	{
+		String color = "Not the last bean!";
+		if (oneLeft())
+		{
+			color = "black";
+			if (getBlack() == 0)
+			{
+				color = "white";
+			}
+		}
+
+		return color;
+	}
+
+	// maybe disable as it later bc redundant and too much access
+	public char[] getBeans() // this may be for drawing/ display purposes
 	{
 		return this.beans;
+	}
+
+	// simulates the drawing of 2 beans
+	// does the required methods
+	// returns a char with the colors of the beans drawn
+	public char[] drawBeans()
+	{
+		char[] draw = createBeans();
+		char bean1 = draw[0];
+		char bean2 = draw[1];
+
+		if (bean1 == 'b' && bean2 == 'b')
+		{
+			optionSameColor('b');
+		}
+		if (bean1 == 'w' && bean2 == 'w')
+		{
+			optionSameColor('w');
+		}
+		else // 'b' & 'w'
+		{
+			optionBothColor();
+		}
+
+		setBeans();
+		return draw;
+	}
+
+	// b will be 0, w will be 1
+	public char[] createBeans()
+	{
+		char[] draw = { 'b', 'b' };
+
+		Random rand = new Random();
+
+		int n = rand.nextInt(this.beans.length);
+		draw[0] = this.beans[n];
+
+		int m = rand.nextInt(this.beans.length);
+		while (m == n)
+		{
+			m = rand.nextInt(this.beans.length);
+		}
+		draw[1] = this.beans[m];
+
+		return draw;
+	}
+
+	// If the two beans are the same color, 
+	// then throw both of them away and add a new black bean. 
+	// translation: color subtract 2, black bean add 1
+	public void optionSameColor(char color)
+	{
+		if (color == 'b')
+		{
+			this.black -= 2;
+		}
+		else //(color == 'w')
+		{
+			this.white -= 2;
+		}
+
+		this.black += 1;
+	}
+
+	// If the two beans are different colors, 
+	// then throw away the black one and return the white one to the can.
+	// translation: decrement 1 black bean
+	public void optionBothColor()
+	{
+		this.black -= 1;
+	}
+
+	// may cause bounds issues, check for if beans are zero
+	public char[] setBeans()
+	{
+		int size = getBlack() + getWhite();
+		char[] beans = new char[size];
+
+		for (int b = 0; b < getBlack(); b++)
+		{
+			beans[b] = 'b';
+		}
+
+		for (int w = getBlack(); w < beans.length; w++)
+		{
+			beans[w] = 'w';
+		}
+
+		return beans;
 	}
 
 }
--- coffeeBeans/src/coffeeBeans/CoffeeBeansMain.java
+++ coffeeBeans/src/coffeeBeans/CoffeeBeansMain.java
@@ -1,6 +1,157 @@
+// marinaescalante-unfinished
+
+// still has errors, cannot run, mostly done
+
+/* planning
+ * simulate beans w/  basic graphics (end game)
+ * choose option, prompt typing? (endgame button)
+ * randomly simulate the 2 bean options (coffee can returns array, does actions)
+ */
+
+// add in name prompt??, variable is ready
+
 package coffeeBeans;
+
+import java.util.Scanner;
 
 public class CoffeeBeansMain
 {
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String name = "";
+		CoffeeBeanCan coffeeCan;
+
+		name = intro(keyboard);
+		coffeeCan = createCan(keyboard);
+
+		while (!coffeeCan.oneLeft())
+		{
+			drawBeans(keyboard, coffeeCan);
+		}
+		endGame(coffeeCan);
+
+	}
+
+	public static String intro(Scanner keyboard)
+	{
+		System.out.println("This is the coffee bean game.");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println("We have a coffee can that contains some white beans and some black beans. ");
+		System.out.println("Here are the rules:");
+		System.out.println("1. Randomly choose two beans. ");
+		System.out.println(
+				"2. If the two beans are the same color," + " then throw both of them away and add a new black bean. ");
+		System.out.println("3. If the two beans are different colors, then throw away the black one and "
+				+ "return the white one to the can.");
+		System.out.println();
+		System.out.print("What is your name? ");
+		String name = keyboard.nextLine();
+		return name;
+	}
+
+	public static CoffeeBeanCan createCan(Scanner keyboard)
+	{
+		System.out.println();
+		System.out.println("Create your coffee bean can. You have 3 options, please type the number you want.");
+		System.out.println("Create a random can for me. <1>");
+		System.out.println("Enter the total amount of random colored beans you want. <2>");
+		System.out.println("Enter specific white and black bean values. <3>");
+		System.out.print("Type: ");
+
+		int type = getType(keyboard);
+
+		CoffeeBeanCan temp;
+
+		if (type == 1)
+		{
+			temp = new CoffeeBeanCan();
+		}
+		else if (type == 2)
+		{
+			System.out.println();
+			System.out.print("How many beans would you like total? ");
+			int beans = keyboard.nextInt();
+			temp = new CoffeeBeanCan(beans);
+		}
+		else //(type == 3)
+		{
+			System.out.println();
+			System.out.print("How many black beans would you like? ");
+			int black = keyboard.nextInt();
+			System.out.print("How many white beans would you like? ");
+			int white = keyboard.nextInt();
+			temp = new CoffeeBeanCan(black, white);
+		}
+
+		return temp;
+	}
+
+	public static void drawBeans(Scanner keyboard, CoffeeBeanCan coffeeCan)
+	{
+		System.out.println();
+		System.out.println("New Turn. Press enter to continue.");
+
+		String ans = keyboard.nextLine();
+		while (!ans.equals("y"))
+		{
+			System.out.print("Would you like to draw 2 beans? Type <y> and press enter to confirm. ");
+			ans = keyboard.nextLine();
+		}
+
+		char[] beans = coffeeCan.drawBeans();
+		displayBeans(beans);
+	}
+
+	public static void displayBeans(char[] beans)
+	{
+		if (beans[0] == beans[1]) // same color
+		{
+			String color = "black";
+			if (beans[0] == 'w')
+			{
+				color = "white";
+			}
+
+			System.out.println("You drew two " + color + "beans!");
+			System.out.println("They were removed. A black bean was added.");
+		}
+
+		else // both colors
+		{
+			System.out.println("You drew a black bean and a white bean!");
+			System.out.println("The black bean was removed.");
+			System.out.println("The white bean was returned to the can.");
+		}
+	}
+
+	public static void endGame(CoffeeBeanCan coffeeCan)
+	{
+		System.out.println("There is one bean left!");
+		System.out.println("The last bean's color is " + coffeeCan.getLastBean());
+	}
+
+	public static int getType(Scanner keyboard)
+	{
+		int type = getNum(keyboard);
+		while (type < 0 || type > 4)
+		{
+			keyboard.nextLine();
+			System.out.println("Please enter a valid type (<1>, <2>, <3>).");
+		}
+		return type;
+	}
+
+	public static int getNum(Scanner keyboard)
+	{
+		while (!keyboard.hasNextInt())
+		{
+			keyboard.nextLine();
+			System.out.println("Please enter a number.");
+		}
+		int num = keyboard.nextInt();
+		return num;
+	}
 
 }
